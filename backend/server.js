85741bfdbd807cd1f8f03cc081f8f7930785c62a
Kaua <rodrigueskaua76@gmail.com>
2025-05-12 20:39:41 -0300

    backend criado

---
+++ backend/server.js
@@ -0,0 +1,66 @@
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const { v4: uuid } = require("uuid")
+
+const app = express()
+const Port = 3000
+
+app.use(cors())
+app.use(bodyParser.json())
+
+let produtos = []
+
+// CREATE
+app.post('/produtos', (req, res) => {
+    const { nome, descricao, descricao2 } = req.body
+
+    if (!nome || !descricao || !descricao2) {
+        return res.status(400).json({ error: 'Todos os campos são obrigatórios.' })
+    }
+
+    const novoItem = { id: uuid(), nome, descricao, descricao2 }
+    produtos.push(novoItem)
+    res.status(201).json(novoItem)
+})
+
+// READ
+app.get('/produtos', (req, res) => {
+    res.json(produtos)
+})
+
+// UPDATE
+app.put('/produtos/:id', (req, res) => {
+    const produtoId = req.params.id
+    const { nome, descricao, descricao2 } = req.body
+
+    if (!nome || !descricao || !descricao2) {
+        return res.status(400).json({ error: 'Todos os campos são obrigatórios.' })
+    }
+
+    const produtoIndex = produtos.findIndex(item => item.id === produtoId)
+    if (produtoIndex === -1) {
+        return res.status(404).json({ error: 'Produto não encontrado.' })
+    }
+
+    produtos[produtoIndex] = { id: produtoId, nome, descricao, descricao2 }
+    res.json(produtos[produtoIndex])
+})
+
+// DELETE
+app.delete('/produtos/:id', (req, res) => {
+    const produtoId = req.params.id
+    const inicioProduto = produtos.length
+    produtos = produtos.filter(item => item.id !== produtoId)
+
+    if (produtos.length === inicioProduto) {
+        return res.status(404).json({ error: 'Produto não encontrado.' })
+    }
+
+    res.status(204).send()
+})
+
+// INICIAR SERVIDOR
+app.listen(Port, () => {
+    console.log(`Servidor rodando na porta ${Port}`)
+})
